5a9354f10f280c9d232090973a2a8170c80a914e
mirhec <mirhec@users.noreply.github.com>
2017-06-12 21:32:29 +0200

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "${#}" -eq 0 ]; then
+  echo Usage: backup.sh [backupdir] [backupname]
+  exit
+fi
+
+BACKUP_DIR=${1:-./backups}
+BACKUP_FILE=${2:-backup.tar.gz}
+BACKUP_DIR=`realpath $BACKUP_DIR`
+
+docker run --rm \
+    -v $BACKUP_DIR:/backups \
+    -v $PWD/database:/var/lib/mysql \
+    -v $PWD/html:/var/www/html \
+    alpine sh -c \
+        "tar cf /tmp/mysql.tar -C /var/lib/mysql --exclude mysqld.sock --exclude *.pid --exclude *logfile* --exclude ibdata* . \
+        && tar cf /tmp/www.tar -C /var/www/html . \
+        && tar czf /backups/$BACKUP_FILE -C /tmp mysql.tar www.tar"
